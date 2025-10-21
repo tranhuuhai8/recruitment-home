e9862c26099213e6e7a994dcffc3a92231337137
tranhuuhai8 <tranhuuhaixtnd852003@gmail.com>
2025-10-21 21:41:14 +0700

    Style page not found

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -8,9 +8,47 @@ const { t } = useI18n()
 
 <template>
     <div className="page-not-found">
-        <p>{{ t('page_not_found.not_found') }}</p>
-        <a-button type="primary" @click="() => router.push({ name: 'home' })">
-            {{ t('page_not_found.btn_redirect') }}
-        </a-button>
+        <div class="stars">
+            <div class="central-body">
+                <h1 class="text-404">{{ t('page_not_found.status') }}</h1>
+                <div
+                    class="btn-go-home"
+                    @click="() => router.push({ name: 'home' })"
+                >
+                    {{ t('page_not_found.btn_redirect') }}
+                </div>
+            </div>
+            <div class="objects">
+                <img
+                    class="object_rocket"
+                    src="http://salehriaz.com/404Page/img/rocket.svg"
+                    width="40px"
+                />
+                <img
+                    class="object_earth"
+                    src="http://salehriaz.com/404Page/img/earth.svg"
+                    width="100px"
+                />
+                <img
+                    class="object_moon"
+                    src="http://salehriaz.com/404Page/img/moon.svg"
+                    width="80px"
+                />
+                <div class="box_astronaut">
+                    <img
+                        class="object_astronaut"
+                        src="http://salehriaz.com/404Page/img/astronaut.svg"
+                        width="140px"
+                    />
+                </div>
+            </div>
+            <div class="glowing_stars">
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+            </div>
+        </div>
     </div>
 </template>
